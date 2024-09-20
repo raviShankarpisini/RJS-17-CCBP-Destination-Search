37886db12bc4ca609437312e26ce6035f2e755f3
Ravi Shankar <ravishankarpisini@gmail.com>
2024-09-20 15:21:57 +0530

    code refractoring

--- src/App.js
+++ src/App.js
@@ -1,92 +1,76 @@
-import {useState} from 'react'
-import DestinationSearch from './components/DestinationSearch'
+import DestinationSearch from "./components/DestinationSearch";
 
-import './App.css'
+import "./App.css";
 
 const destinationsList = [
   {
     id: 1,
-    name: 'Melaka Mosque',
-    imgUrl: 'https://assets.ccbp.in/frontend/react-js/melaka-mosque-img.png',
+    name: "Melaka Mosque",
+    imgUrl: "https://assets.ccbp.in/frontend/react-js/melaka-mosque-img.png",
   },
   {
     id: 2,
-    name: 'Shrubland',
-    imgUrl: 'https://assets.ccbp.in/frontend/react-js/shrubland-img.png',
+    name: "Shrubland",
+    imgUrl: "https://assets.ccbp.in/frontend/react-js/shrubland-img.png",
   },
   {
     id: 3,
-    name: 'New York',
-    imgUrl: 'https://assets.ccbp.in/frontend/react-js/new-york-img.png',
+    name: "New York",
+    imgUrl: "https://assets.ccbp.in/frontend/react-js/new-york-img.png",
   },
   {
     id: 4,
-    name: 'Escarpment',
-    imgUrl: 'https://assets.ccbp.in/frontend/react-js/escarpment-img.png',
+    name: "Escarpment",
+    imgUrl: "https://assets.ccbp.in/frontend/react-js/escarpment-img.png",
   },
   {
     id: 5,
-    name: 'Westminster Abbey',
+    name: "Westminster Abbey",
     imgUrl:
-      'https://assets.ccbp.in/frontend/react-js/westminster-abbey-img.png',
+      "https://assets.ccbp.in/frontend/react-js/westminster-abbey-img.png",
   },
   {
     id: 6,
-    name: 'South Downs National Park',
+    name: "South Downs National Park",
     imgUrl:
-      'https://assets.ccbp.in/frontend/react-js/south-downs-national-park-img.png',
+      "https://assets.ccbp.in/frontend/react-js/south-downs-national-park-img.png",
   },
   {
     id: 7,
-    name: 'National Historic Site',
+    name: "National Historic Site",
     imgUrl:
-      'https://assets.ccbp.in/frontend/react-js/national-historic-site-img.png',
+      "https://assets.ccbp.in/frontend/react-js/national-historic-site-img.png",
   },
   {
     id: 8,
-    name: 'Tower Bridge',
-    imgUrl: 'https://assets.ccbp.in/frontend/react-js/tower-bridge-img.png',
+    name: "Tower Bridge",
+    imgUrl: "https://assets.ccbp.in/frontend/react-js/tower-bridge-img.png",
   },
   {
     id: 9,
-    name: 'Arc Here',
-    imgUrl: 'https://assets.ccbp.in/frontend/react-js/arc-here-img.png',
+    name: "Arc Here",
+    imgUrl: "https://assets.ccbp.in/frontend/react-js/arc-here-img.png",
   },
   {
     id: 10,
-    name: 'Steeple',
-    imgUrl: 'https://assets.ccbp.in/frontend/react-js/steeple-img.png',
+    name: "Steeple",
+    imgUrl: "https://assets.ccbp.in/frontend/react-js/steeple-img.png",
   },
   {
     id: 11,
-    name: 'Glaciokarst',
-    imgUrl: 'https://assets.ccbp.in/frontend/react-js/glaciokarst-img.png',
+    name: "Glaciokarst",
+    imgUrl: "https://assets.ccbp.in/frontend/react-js/glaciokarst-img.png",
   },
   {
     id: 12,
-    name: 'Parco Nazionale delle Cinque Terre',
+    name: "Parco Nazionale delle Cinque Terre",
     imgUrl:
-      'https://assets.ccbp.in/frontend/react-js/parco-nazionale-delle-cinque-terre-img.png',
+      "https://assets.ccbp.in/frontend/react-js/parco-nazionale-delle-cinque-terre-img.png",
   },
-]
+];
 
 const App = () => {
-  const [searchInput, setSearchInput] = useState('')
-  const onChangeSearchInput = event => {
-    setSearchInput(event.target.value)
-  }
-  const filteredDestinations = destinationsList.filter(destination =>
-    destination.name.toLowerCase().includes(searchInput.toLowerCase()),
-  )
+  return <DestinationSearch destinationsList={destinationsList} />;
+};
 
-  return (
-    <DestinationSearch
-      destinationsList={filteredDestinations}
-      searchInput={searchInput}
-      onChangeSearchInput={onChangeSearchInput}
-    />
-  )
-}
-
-export default App
-
+export default App;
